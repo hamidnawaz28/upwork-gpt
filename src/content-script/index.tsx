99564f09761de2060c9b739ec71f56c92bef7e3d
Hamid Nawaz <hamid.nawaz@icontainers.com>
2023-07-25 02:29:24 +0500

    fixed no popup show button

--- src/content-script/index.tsx
+++ src/content-script/index.tsx
@@ -3,21 +3,17 @@ import '../../base.css'
 import ResponseContainer from './components/ResponseContainer'
 
 const renderBidButton = () => {
-  let coverLetterArea = null
   let mainContainer = null
   const timeInterval = setInterval(() => {
-    coverLetterArea = document.querySelector('#cover_letter_label')
     mainContainer = document.querySelector('#main')
-    if (coverLetterArea !== null && mainContainer !== null) {
+    if (mainContainer !== null) {
       clearInterval(timeInterval)
       const responseContainer = document.createElement('div')
       mainContainer.append(responseContainer)
       const moreDescription = document.querySelector('.up-truncation-label') as HTMLElement
-
       if (moreDescription) {
-        moreDescription.click()
+        moreDescription?.click()
       }
-
       const root = createRoot(responseContainer)
       root.render(<ResponseContainer />)
     }
