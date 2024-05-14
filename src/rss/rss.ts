2549e79dc03792e520cf2f1b3b8b30511fa5d6e4
hamidnawaz <hamid.nawaz28@yahoo.com>
2024-05-14 15:56:56 +1000

    feat: formated fields

--- src/rss/rss.ts
+++ src/rss/rss.ts
@@ -1,3 +1,4 @@
+// example
 const rssString2 = `https://www.upwork.com/ab/feed/jobs/rss?
 budget=0-99%2C100-499%2C500-999%2C1000-4999%2C5000-
 &client_hires=0%2C1-9%2C10-
@@ -21,117 +22,153 @@ budget=0-99%2C100-499%2C500-999%2C1000-4999%2C5000-
 &userUid=1124677144079728640
 &orgUid=1124677144083922945`
 
-const upworkData = {
-  budget: {
-    name: 'budget',
-    type: 'multi-select',
-    data: ['0-99', '100-499', '500-999', '1000-4999', '5000-'],
-  },
-  clientHistoricalHires: {
-    name: 'client_hires',
-    type: 'multi-select',
-    data: [
-      // hostorical hiring from client
-      '0',
-      '1-9',
-      '10-',
-    ],
-  },
-  connectPrice: {
-    name: 'connect_price',
-    type: 'multi-select',
-    data: ['0-4', '8', '12', '16'],
-  },
-  expereienceLevelRequired: {
-    name: 'contractor_tier',
-    type: 'multi-select',
-    data: ['1', '2', '3'],
-  },
+enum FieldType {
+  SELECT,
+  MULTISELECT,
+  BOOLEAN,
+  RANGE,
+  TEXT,
+}
 
+const budget = {
+  name: 'budget',
+  label: 'Budget',
+  type: FieldType.MULTISELECT,
+  options: ['0-99', '100-499', '500-999', '1000-4999', '5000-'],
+}
+
+const clientHistoricalHires = {
+  name: 'client_hires',
+  label: 'Client Historical Hires',
+  type: FieldType.MULTISELECT,
+  options: ['0', '1-9', '10-'],
+}
+
+const connectPrice = {
+  name: 'connect_price',
+  label: 'Connect Price',
+  type: FieldType.MULTISELECT,
+  options: ['0-4', '8', '12', '16'],
+}
+
+const expereienceLevelRequired = {
+  name: 'contractor_tier',
+  label: 'Your level',
+  type: FieldType.MULTISELECT,
+  options: ['1', '2', '3'],
+}
+
+const projectDurations = {
+  name: 'duration_v3',
+  label: 'Project Duration',
+  type: FieldType.MULTISELECT,
+  options: ['week', 'month', 'semester', 'ongoing'],
+}
+
+const hourlyRate = {
+  name: 'hourly_rate',
+  label: 'Hourly Rate',
+  type: FieldType.RANGE,
+  range: {
+    min: 1,
+    max: 1000,
+  },
+}
+
+const country = {
+  name: 'location',
+  label: 'Country',
+  type: FieldType.MULTISELECT,
+  options: [
+    // complete list
+    'Albania',
+    'Australia+and+New+Zealand',
+    'Africa',
+  ],
+}
+
+const noOfProposals = {
+  name: 'proposals',
+  label: 'Proposals',
+  type: FieldType.MULTISELECT,
+  options: ['0-4', '5-9', '10-14', '15-19', '20-49'],
+}
+
+const keywords = {
+  // verify we can use multiple
+  name: 'q',
+  label: 'Keywords',
+  type: FieldType.TEXT,
+  text: '',
+}
+
+const sort = {
+  name: 'sort',
+  label: 'Sort By',
+  type: FieldType.SELECT,
+  options: [
+    // add other options as well
+    'recency', // prefered
+    'relevance+desc',
+    'client_total_charge',
+    'client_rating+desc',
+  ],
+}
+
+const rateType = {
+  name: 'job_type',
+  label: 'Rate Type',
+  type: FieldType.MULTISELECT,
+  options: ['hourly', 'fixed'],
+}
+
+const jobType = {
+  name: 'workload',
+  label: 'Job Type',
+  type: FieldType.MULTISELECT,
+  options: ['as_needed', 'part_time', 'full_time'],
+}
+
+const pagingation = {
+  name: 'paging',
+  label: 'Pagination',
+  type: FieldType.RANGE,
+  range: [
+    // verify the logic
+    '0', // from 0%3B50 like 0;50
+    '50', // to
+  ],
+}
+
+const timezone = {
+  name: 'timezone',
+  label: 'Timezone',
+  type: FieldType.MULTISELECT,
+  options: [
+    // complete list
+    'Pacific/Midway',
+    'Pacific/Honolulu',
+  ],
+}
+
+const upworkData = {
+  budget,
+  clientHistoricalHires,
+  connectPrice,
+  expereienceLevelRequired,
   contract_to_hire: true,
   verified_payment_only: 1,
   previous_clients: 1, // verify
-
-  projectDurations: {
-    name: 'duration_v3',
-    type: 'multi-select',
-    data: ['week', 'month', 'semester', 'ongoing'],
-  },
-
-  hourlyRate: {
-    name: 'hourly_rate',
-    type: 'fields',
-    data: {
-      min: 1,
-      max: 1000,
-    },
-  },
-
-  location: {
-    name: 'location',
-    type: 'multi-select',
-    data: [
-      // complete list
-      'Albania',
-      'Australia+and+New+Zealand',
-      'Africa',
-    ],
-  },
-
-  noOfProposals: {
-    name: 'proposals',
-    type: 'multi-select',
-    data: ['0-4', '5-9', '10-14', '15-19', '20-49'],
-  },
-  keywords: {
-    // verify we can use multiple
-    name: 'q',
-    type: 'text-field',
-    data: 'chrome+extension',
-  },
-
-  sort: {
-    name: 'sort',
-    type: 'single-select',
-    data: [
-      // add other options as well
-      'recency', // prefered
-      'relevance+desc',
-      'client_total_charge',
-      'client_rating+desc',
-    ],
-  },
-  jobType: {
-    name: 'job_type',
-    type: 'multi-select',
-    data: ['hourly', 'fixed'],
-  },
-  workload: {
-    name: 'workload',
-    type: 'multi-select',
-    data: ['as_needed', 'part_time', 'full_time'],
-  },
-
-  pagingation: {
-    name: 'paging',
-    type: 'number-fields',
-    data: [
-      // verify the logic
-      '0', // from 0%3B50 like 0;50
-      '50', // to
-    ],
-  },
-
-  timezone: {
-    name: 'timezone',
-    type: 'multi-select',
-    data: [
-      // complete list
-      'Pacific/Midway',
-      'Pacific/Honolulu',
-    ],
-  },
-
+  projectDurations,
+  hourlyRate,
+  location: country,
+  noOfProposals,
+  keywords,
+  sort,
+  rateType,
+  jobType,
+  pagingation,
+  timezone,
   api_params: 1,
   securityToken:
     '5534860065db8587ad93d99f828992a2630fd9dca29564c02b612cd1688a5fc3941cf6b85fc14d529dc4f2270de5d957d7b810dde33a2e9dd8d3e996f40aa0bc',
